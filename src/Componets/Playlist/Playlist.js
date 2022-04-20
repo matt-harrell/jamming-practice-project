436bdf2d443309b33d620596154a1e66295a1380
Matt Harrell <matthew.629@hotmail.com>
2022-04-19 22:35:27 -0400

    add user can change playlist state name

--- src/Componets/Playlist/Playlist.js
+++ src/Componets/Playlist/Playlist.js
@@ -4,10 +4,19 @@ import TrackList from "../TrackList/TrackList";
 
 
 class Playlist extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleNameChange(e){
+        let newPlaylistName = e.target.value;
+        this.props.onNameChange(newPlaylistName)
+    }
+
     render(){
         return (
-            <div className="Playlist">
-                <input defaultValue={'New Playlist'} />
+            <div className="Playlist" >
+                <input defaultValue={'New Playlist'}  onChange={this.handleNameChange}/>
                 <TrackList  
                     tracks={this.props.playlistTracks}
                     onRemove={this.props.onRemove}
