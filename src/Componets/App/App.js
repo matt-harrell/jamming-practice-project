be034149a67fe12f25ae20167810c6f3187fd603
Matt Harrell <matthew.629@hotmail.com>
2022-04-20 23:05:03 -0400

    add save playlist method

--- src/Componets/App/App.js
+++ src/Componets/App/App.js
@@ -55,6 +55,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track){
@@ -77,6 +78,10 @@ class App extends React.Component {
     this.setState({playlistName:name})
   }
 
+  savePlaylist(){
+    const trackURIs = this.state.playlistTracks.map(track => track.uri)
+  }
+
   render() {
     return (
       <div>
@@ -95,6 +100,7 @@ class App extends React.Component {
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
